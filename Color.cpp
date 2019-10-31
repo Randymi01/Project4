6e23c7d43b8b1108cf9b35555baeec4aa66c119c
Randy Mi <53793743+Randymi01@users.noreply.github.com>
2019-10-30 20:14:58 -0400

    a

---
+++ Color.cpp
@@ -0,0 +1,34 @@
+/**
+ * Color.cpp
+ * Project UID 8b3bcc444eb500121e420f7e2e359014
+ *
+ * EECS 183, Fall 2019
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Color.h"
+
+// TODO: implement first checkRange, then two constructors, setRed, getRed,
+//       setGreen, getGreen, setBlue, getBlue, read, write.
+
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+
+istream& operator >> (istream& ins, Color& color)
+{
+   color.read(ins);
+   return ins;
+}
+
+ostream& operator << (ostream& outs, Color color)
+{
+   color.write(outs);
+   return outs;
+}
---
+++ Point.cpp
@@ -0,0 +1,36 @@
+/**
+ * Point.cpp
+ * Project UID 8b3bcc444eb500121e420f7e2e359014
+ *
+ * EECS 183, Fall 2019
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Point.h"
+
+// for the declaration of DIMENSION
+#include "utility.h"
+
+// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
+
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+
+istream& operator >> (istream& ins,  Point& pt)
+{
+    pt.read(ins);
+    return ins;
+}
+
+ostream& operator<< (ostream& outs, Point pt)
+{
+    pt.write(outs);
+    return outs;
+}
